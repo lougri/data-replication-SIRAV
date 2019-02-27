bfa979afef08a451daea707421ee25038a765cc2
Guillaume Lipski <guillaume.lipski@orange.fr>
2019-02-27 16:06:01 +0100

    ajout de commentaires & questions dans le code python

--- projet.py
+++ projet.py
@@ -15,29 +15,39 @@ On dirait que dirtyA correspond à l'écart (aux différences) entre O et A. Pou
 
 class LocalFileSystem :
     #def __init__(self,...) == creation de classe
+    def __init(self,path):
+        root=path
+
+    #retourne la racine de l'os
+    #TODO resultat change en fonction de l'OS, uniquement linux ici
     def rootOfFileSystem(self):
         return '/'
 
+    #retourne la racine du dossier à synchroniser ? si oui, inutile de créer de get pour cet attribut, pas de private en python !
+    #actuellement retourne la même chose que rootOfFileSystem
     def getRoot(self):
         return self.rootOfFileSystem()
 
-
+    #Récupère le chemin du parent du fichier/dossier path, si c'est la racine du dossier a traiter, retourne le même résultat
     def getParent(self,path):
+        #lipski
+        if(path==self.root):
+            return path
+        else
         # Louis Grison
-        return (  (path.rsplit('/',1))[0] )
+            return ((path.rsplit('/',1))[0])
 
+    #liste le contenu du dossier path
     def getChildren(self, path):
         # Louis Grison
         #returns the list of files and folders that are in the directory
         return os.listdir(path)
 
+    # remplace le fichier absolutePathTargetFileSystem par absolutePathSourceFileSystem (que fais fsSource?)
     def replace(self, absolutePathTargetFileSystem, fsSource, absolutePathSourceFileSystem):
         return ''
 
-lfs=LocalFileSystem()
-
-
-
+lfs=LocalFileSystem("/")
 
 
 
